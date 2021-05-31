31897ce041ec3bf1ef0ba025a151de0948552eb3
Rafael Mattioli <rbmattioli@gmail.com>
2021-05-30 22:34:27 -0300

    testando o rest

---
+++ src/main/java/br/com/mattioli/cursoms/resources/categoriaResources.java
@@ -0,0 +1,20 @@
+package br.com.mattioli.cursoms.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class categoriaResources {
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		
+		return "rest esta funcionando";
+	}
+	
+	
+
+}
