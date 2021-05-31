46dde78e50a2e5fea5863e38d4dfb4e77450d276
Rafael Mattioli <rbmattioli@gmail.com>
2021-05-30 23:05:09 -0300

    primeira classe dominio

--- src/main/java/br/com/mattioli/cursoms/resources/categoriaResources.java
+++ src/main/java/br/com/mattioli/cursoms/resources/categoriaResources.java
@@ -1,18 +1,30 @@
 package br.com.mattioli.cursoms.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.mattioli.cursoms.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
-public class categoriaResources {
+public class CategoriaResources {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "rest esta funcionando";
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCategoria = new ArrayList<>();
+		listaCategoria.add(cat);
+		listaCategoria.add(cat2);
+		
+		return listaCategoria;
 	}
 	
 	
