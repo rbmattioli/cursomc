7d13767af0a8ef996034fc573c981322d8d1aa73
Rafael Mattioli <rbmattioli@gmail.com>
2021-05-30 22:09:15 -0300

    projeto criado

---
+++ src/main/java/br/com/mattioli/cursoms/CursomsApplication.java
@@ -0,0 +1,13 @@
+package br.com.mattioli.cursoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mattioli/cursoms/CursomsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mattioli.cursoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
