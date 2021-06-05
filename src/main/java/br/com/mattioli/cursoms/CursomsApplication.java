a47ae68a1bf4a0b85d74c81c47f6af85efa82bc6
Rafael Mattioli <rbmattioli@gmail.com>
2021-06-05 14:06:49 -0300

    criando ope de instaciacao

--- src/main/java/br/com/mattioli/cursoms/CursomsApplication.java
+++ src/main/java/br/com/mattioli/cursoms/CursomsApplication.java
@@ -1,13 +1,35 @@
 package br.com.mattioli.cursoms;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.mattioli.cursoms.domain.Categoria;
+import br.com.mattioli.cursoms.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomsApplication {
+public class CursomsApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomsApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
+	
+	
+	
 
 }
