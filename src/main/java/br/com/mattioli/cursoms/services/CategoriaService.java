ca28c3e9cf8e51be59021ee6e9d7ca8de84f5283
Rafael Mattioli <rbmattioli@gmail.com>
2021-06-06 13:01:19 -0300

    Atualizando uma categoria com PUT

--- src/main/java/br/com/mattioli/cursoms/services/CategoriaService.java
+++ src/main/java/br/com/mattioli/cursoms/services/CategoriaService.java
@@ -13,7 +13,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 
 		Categoria obj = repo.getById(id);
 
@@ -26,10 +26,16 @@ public class CategoriaService {
 		return obj;
 
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+
 	}
 
 }
