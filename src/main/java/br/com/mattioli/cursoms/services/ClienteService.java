3b0c9ae05c052f19ef017ef9ba0b476998bed0d7
Rafael Mattioli <rbmattioli@gmail.com>
2021-06-06 23:32:51 -0300

    Put Delete Get para cliente

--- src/main/java/br/com/mattioli/cursoms/services/ClienteService.java
+++ src/main/java/br/com/mattioli/cursoms/services/ClienteService.java
@@ -1,10 +1,18 @@
 package br.com.mattioli.cursoms.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.mattioli.cursoms.domain.Cliente;
+import br.com.mattioli.cursoms.dto.ClienteDTO;
 import br.com.mattioli.cursoms.repositories.ClienteRepository;
+import br.com.mattioli.cursoms.services.exceptions.DataIntegrityException;
 import br.com.mattioli.cursoms.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -25,6 +33,48 @@ public class ClienteService {
 		}
 		return obj;
 
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+
+			throw new DataIntegrityException("Não é possível excluir um cliente que possiu pedidos");
+
+		}
+
+	}
+
+	public List<Cliente> findAll() {
+
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String direction,  String orderBy) {
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO (ClienteDTO objDTO) {
+		
+		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null);
+		
+	}
+	
+	private void updateData (Cliente newobj, Cliente obj) {
+		newobj.setNome(obj.getNome());
+		newobj.setEmail(obj.getEmail());
 	}
 
 }
