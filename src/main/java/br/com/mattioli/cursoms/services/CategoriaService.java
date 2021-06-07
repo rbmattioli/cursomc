6639516612ac1133541bf37eb7b4dd7c1680fb22
Rafael Mattioli <rbmattioli@gmail.com>
2021-06-07 09:25:03 -0300

    Correcoes Upadate em categoria service e contrutor de pagamento

--- src/main/java/br/com/mattioli/cursoms/services/CategoriaService.java
+++ src/main/java/br/com/mattioli/cursoms/services/CategoriaService.java
@@ -41,8 +41,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 
 	}
 
@@ -73,6 +74,12 @@ public class CategoriaService {
 	public Categoria fromDTO (CategoriaDTO objDTO) {
 		
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+		
+	}
+	
+	private void updateData(Categoria newObj,Categoria obj ) {
+		
+		newObj.setNome(obj.getNome());
 		
 	}
 	
